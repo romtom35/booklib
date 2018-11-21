8492e7deff522c5fa5f763952ab6f66551a35f6c
romtom35 <romtom35@gmail.com>
2018-11-21 09:10:09 +0100

    ajout de route

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Author;
+use App\Entity\Book;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class DefaultController
+ * @package App\Controller
+ * @Route("/default")
+ */
+class DefaultController extends BaseController
+{
+    /**
+     * @Route("/{nom}", name="default", methods={"GET"})
+     */
+    public function index(string $nom, Request $request)
+    {
+        $author = $this->getDoctrine()->getRepository(Author::class)->findOneBy(['lastname' => $nom]);
+
+        if(!$author){
+            throw $this->createNotFoundException('Auteur introuvable');
+        }
+
+        return new Response($author->getLastname() . ' ' . $author->getFirstname() . ' ' . $author->getId());
+
+    }
+
+    /**
+     * @Route("/book/{slug}", name="show-book")
+     */
+    public function showBook(Book $book){
+        return new Response($book->getTitle());
+    }
+}

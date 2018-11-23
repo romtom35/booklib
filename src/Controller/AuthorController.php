d5f550a8d40526a722093695238127bf3945d700
Romain THOMAS <r_thomas3@digital-campus.net>
2018-11-23 09:32:19 +0100

    creation de formulaires

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -2,8 +2,43 @@
 
 namespace App\Controller;
 
+use App\Entity\Author;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 
-class AuthorController
+/**
+ * Class AuthorController
+ * @package App\Controller
+ * @Route("/author")
+ */
+class AuthorController extends BaseController
 {
+    /**
+     * @Route("/new", name="author_new")
+     */
+    public function new(Request $request)
+    {
+        $author = new Author();
+
+        $form = $this->createFormBuilder($author)
+            ->add('firstname', TextType::class)
+            ->add('lastname', TextType::class)
+            ->add('save', SubmitType::class, array('label' => 'Enregistrer'))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($author);
+            $em->flush();
+
+            return $this->redirectToRoute('homepage');
+        }
+
+        return $this->render('author/new.html.twig', ['form' => $form->createView()]);
+    }
 
 }

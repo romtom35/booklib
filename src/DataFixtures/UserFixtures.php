0801651f814c36da521bfe58ee8b2decf4cfd753
romtom35 <romtom35@gmail.com>
2018-11-23 14:48:21 +0100

    creation de la partie login/creation de compte

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -6,14 +6,42 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use App\Entity\User;
+use Faker\Factory;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserFixtures extends Fixture
 {
 
+    private $passwordEncoder;
+
+    /**
+     * UserFixtures constructor.
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     */
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
     public function load(ObjectManager $manager)
     {
+        $admin = new User();
+        $admin->setFirstname('Romain');
+        $admin->setLastname('Thomas');
+        $admin->setEmail('romtom35@gmail.com');
+        $admin->setPassword($this->passwordEncoder->encodePassword($admin, 'rthomas'));
+        $admin->setRoles(['ROLE_ADMIN']);
+        $manager->persist($admin);
+
+        $faker = Factory::create('fr_FR');
+
         for ($i = 0; $i < 20; $i++) {
             $user = new User();
+            $user->setFirstname($faker->firstName);
+            $user->setLastname($faker->lastName);
+            $user->setEmail($faker->email);
+            $user->setPassword($this->passwordEncoder->encodePassword($user, '1234'));
+            $user->setRoles(['ROLE_USER']);
             $manager->persist($user);
             $this->addReference('user-' . ($i + 1), $user);
         }
